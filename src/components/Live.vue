fe37b99050c872bc9280cf7a1d2c4f56bf5c4a1e
czx.me <heycmm.cn@gmail.com>
2022-12-28 12:36:02 +0800

    change：相对路径拉动态流地址

--- src/components/Live.vue
+++ src/components/Live.vue
@@ -1,7 +1,7 @@
 <template>
-    <vue-plyr class="container" ref="plyr" :options="playerOptions">
-        <video></video>
-    </vue-plyr>
+  <vue-plyr class="container" ref="plyr" :options="playerOptions">
+    <video></video>
+  </vue-plyr>
 </template>
 
 <script>
@@ -10,32 +10,45 @@ import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
 
 export default {
-    name: 'Live',
-    components: {
-        VuePlyr
+  name: 'Live',
+  components: {
+    VuePlyr
+  },
+  data() {
+    return {
+      hlsUrl: process.env.VUE_APP_HLS_URL,
+      playerOptions: {
+        controls: ['play-large', 'play', 'mute', 'volume', 'fullscreen'],
+        settings: ['quality', 'speed', 'loop'],
+      }
+    }
+  },
+  computed: {
+    player() {
+      return this.$refs.plyr.player;
     },
-    data() {
-        return {
-            hlsUrl: process.env.VUE_APP_HLS_URL,
-            playerOptions: {
-                controls: ['play-large', 'play', 'mute', 'volume', 'fullscreen'],
-                settings: ['quality', 'speed', 'loop'],
-            }
-        }
-    },
-    computed: {
-        player() {
-            return this.$refs.plyr.player;
-        },
-    },
-    mounted() {
-        if (Hls.isSupported()) {
-            const hls = new Hls();
-            hls.loadSource(this.hlsUrl);
-            hls.attachMedia(this.player.media);
-            window.hls = hls;
-        }
-    },
+    GetUrlRelativePath() {
+      const url = document.location.toString();
+      const arrUrl = url.split("//");
+      const start = arrUrl[1].indexOf("/");
+      let relUrl = arrUrl[1].substring(start);
+      if (relUrl.indexOf("?") !== -1) {
+        relUrl = relUrl.split("?")[0];
+      }
+      return relUrl;
+    }
+  },
+  mounted() {
+    if (this.GetUrlRelativePath){
+      this.hlsUrl=this.hlsUrl+this.GetUrlRelativePath+'/hls.m3u8'
+    }
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(this.hlsUrl);
+      hls.attachMedia(this.player.media);
+      window.hls = hls;
+    }
+  },
 
 }
 </script>
